8129a74f041199469f49af36fe022620330cca60
M. Tissot Evan <tissot@insa-07446>
2017-10-23 10:49:07 +0200

    Scroll + Text

--- countdown.js
+++ countdown.js
@@ -1,4 +1,45 @@
 
+/*var imageScrollDivs = document.getElementsByClassName('slider-change');
+
+for(var i=0;i<imageScrollDivs.length;++i)
+{
+	imageScrollDivs[i].onclick = function(event) {
+
+		// This will prevent the default action of the anchor
+		//event.preventDefault();
+
+		//window.scrollTo(100, 0);
+		//window.location.hash.show();
+
+    	window.scrollTop(0);
+
+		// Failing the above, you could use this, however the above is recommended
+		//return false;
+
+	};
+}*/
+
+$('a[href*=\\#]').on('click', function(event){
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
 
 
 var countDownDate = new Date("Jan 27, 2018 9:00:00").getTime();
