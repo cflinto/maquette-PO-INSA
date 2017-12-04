ef6a2c708d09b8c175438274bd1e7fba96e8e5ed
M. Tissot Evan <tissot@insa-20237>
2017-12-04 10:39:06 +0100

    Modif oklm

--- countdown.js
+++ countdown.js
@@ -42,34 +42,68 @@ $(document).on('click', 'a[href^="#"]', function(event) {
 window.setTimeout(offsetAnchor, 0);
 
 
+
+var image_num = 3;
+var current_image = 1;
+var last_image_change = 0;
+
 var countDownDate = new Date("Jan 27, 2018 9:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
+    ++last_image_change;
 
-  // Get todays date and time
-  var now = new Date().getTime();
+    // Get todays date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("counter").innerHTML = days + "J " + hours + "H "
-  + minutes + "M " + seconds + "S ";
+    // Display the result in the element with id="demo"
+    document.getElementById("counter").innerHTML = days + "J " + hours + "H "
+    + minutes + "M " + seconds + "S ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("counter").innerHTML = "EXPIRED";
-  }
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("counter").innerHTML = "Maintenant";
+    }
+
+    if(last_image_change > 7)
+    {
+        ++current_image;
+        if(current_image > image_num)
+        {
+            current_image = 1;
+        }
+        set_image(current_image);
+    }
 }, 1000);
 
+function set_image(num)
+{
+    var elem = document.getElementById('image-holder');
+    current_image = num;
+    last_image_change = 0;
+    if(num == 1)
+    {
+        elem.setAttribute('style', 'left:0px;');
+    }
+    else if(num == 2)
+    {
+        elem.setAttribute('style', 'left:-800px;');
+    }
+    else if(num == 3)
+    {
+        elem.setAttribute('style', 'left:-1600px;');
+    }
+}
 
-
+set_image(1);
 
